063de2a5f0fd7e3fcde7df322efffb6590e0236a
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-03-14 14:35:40 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,32 @@
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-import { nanoid } from 'nanoid';
-import { addContact, deleteContact } from 'redux/contactsSlice';
-import { addFilter } from 'redux/filterSlice';
 
-import { getContacts, getFilter } from 'redux/selectors';
+import { addFilter } from 'redux/filterSlice';
+import { fetchContacts, addContact, deleteContact } from 'redux/operations';
+import {
+  getContacts,
+  getFilter,
+  getError,
+  getIsLoading,
+} from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
   const contactsList = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   const onHandleSubmit = (name, number) => {
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    const findContact = contactsList.find(contact => contact.name === name);
-    if (findContact) {
-      alert(`${name} is already in contacts`);
-    } else {
-      dispatch(addContact(newContact));
-    }
+    dispatch(addContact({ name, number }));
   };
 
   const onFilterChange = e => {
@@ -47,7 +48,8 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm onHandleSubmit={onHandleSubmit} />
       <h2>Contacts</h2>
-      <Filter onFilterChange={onFilterChange} filter={filter} />
+      <Filter onFilterChange={onFilterChange} filter={filter} /> <br />
+      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList
         filteredContacts={filteredContacts()}
         deleteItem={deleteItem}
